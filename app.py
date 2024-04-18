cc81e877bcb25328e2f6344f2374354c7df46cdf
priyanshusingh2023 <139940149+priyanshusingh2023@users.noreply.github.com>
2024-04-18 10:24:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,24 +13,44 @@ class ChatService:
         self.api_key = api_key
         self.base_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + self.api_key
 
-    def get_interview_questions(self, domain, role, difficulty_level, specific_topic, num_questions=15):
+    def get_interview_questions(self, domain, role, difficulty_level, specific_topic, num_questions=10):
         try:
             headers = {'Content-Type': 'application/json'}
 
             prompt = f"You are a {role} in the {domain} domain preparing interview questions."
 
             prompt += f"\n\nGenerate {num_questions} interview questions along with answers for the topic of {specific_topic} with difficulty level {difficulty_level}."
-            prompt += f"\nReturn your response in an array of Json Objects. Each Object will have question and answer keys. "
-            prompt += f"Below is the example structure that you should return your response: "
+            prompt += f"\nOut of these, include 3-5 scenario-based questions."
+            prompt += f"\nProvide direct answers without suggestions or advice. Give the answer in first person."
+            prompt += f"\nReturn your response in an array of JSON objects. Each object will have 'question' and 'answer' keys."
+            prompt += f"\nBelow is the example structure that you should return your response:"
             prompt += f'''[
               {{
                 "question": "What is @SpringBootApplication annotation used for in Spring Boot?",
-                "answer": "The @SpringBootApplication annotation is a convenience annotation that combines @Configuration, @EnableAutoConfiguration, and @ComponentScan annotations. It is used to mark a class as the main Spring Boot application class and is typically placed on the main class of the application."
+                "answer": "The @SpringBootApplication annotation is used to mark a class as the main Spring Boot application class and is typically placed on the main class of the application."
               }},
               {{
                 "question": "Explain the concept of dependency injection in Spring Boot.",
-                "answer": "Dependency injection is a technique where an object is given its dependencies by external code rather than creating them itself. In Spring Boot, dependency injection is achieved through annotations like @Autowired which allows the Spring IoC container to automatically wire dependencies into beans."
-              }}
+                "answer": "Dependency injection is a technique where an object is given its dependencies by external code rather than creating them itself."
+              }},
+              {{
+                "question": "You encounter a circular dependency issue in a Spring Boot project. How would you resolve it?",
+                "answer": "One way to resolve a circular dependency issue in Spring Boot is by using constructor injection instead of field injection. This allows dependencies to be injected via constructor parameters, avoiding the circular reference problem."
+              }},
+              {{
+                "question": "You are tasked with optimizing the performance of a Spring Boot application. What steps would you take?",
+                "answer": "To optimize the performance of a Spring Boot application, I will analyze and optimize database queries, implement caching mechanisms, and utilize asynchronous processing where applicable."
+              }},
+              {{
+            "question": "In a Spring Boot application, how do you handle data validation using the Bean Validation API?",
+            "answer": "To utilize the Bean Validation API for data validation, I will add the 'hibernate-validator' dependency and annotate data fields with constraints defined in the javax.validation package. Spring Boot will automatically perform validation during object creation, providing detailed error messages for invalid data.",
+            
+        }},
+        {{
+        "question": "You are working on a Spring Boot application that uses an in-memory database. What steps would you take to ensure data persistence across application restarts?",
+            "answer": "To ensure data persistence in Spring Boot applications using an in-memory database, I will consider implementing a data persistence mechanism such as using a persistent database (e.g., MySQL, PostgreSQL) or incorporating a caching solution (e.g., Redis, Memcached) to store data.",
+            
+        }}
             ]'''
 
             generation_config = {
@@ -73,7 +93,7 @@ def generate_questions():
     role = request_data.get('role')
     difficulty_level = request_data.get('difficulty_level')
     specific_topic = request_data.get('specific_topic')
-    num_questions = request_data.get('num_questions', 15)  # Default to 10 questions if not specified
+    num_questions = request_data.get('num_questions', 10)  # Default to 10 questions if not specified
 
     # Generate interview questions using ChatService
     api_key = "AIzaSyCYutjs2BzQThKnA2q1hDNbZro4Al7N0Dw"  # Replace 'YOUR_API_KEY' with your actual API key
